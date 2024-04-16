5142c106b9034cd97812dac166eb47551c52fdc8
Bradleykibs <Bradleykibwana@gmail.com>
2024-04-16 19:39:37 +0300

    Initial setup for the project

---
+++ APIs-demo/README.md
@@ -0,0 +1,10 @@
+# A simple Inventory management api made with fastapi
+
+## It demonstrates basic principles of building an api.
+
+- API naming convention
+- Authorization
+- Authentication
+- Pagination
+- Filtering
+- Sorting
---
+++ APIs-demo/main.py
@@ -0,0 +1,10 @@
+import uvicorn
+
+from config import config
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app="app:app",
+        reload=True if config.ENVIRONMENT != "production" else False,
+        workers=1,
+    )
